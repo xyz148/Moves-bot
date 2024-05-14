a35900b9cc3be0e6f19ff0486b2b3463fbd55a53
xyz148 <zxy30730@gmail.com>
2024-05-15 00:26:27 +0530

    Update carbon.py

--- plugins/ExtraMods/carbon.py
+++ plugins/ExtraMods/carbon.py
@@ -30,7 +30,7 @@ async def carbon_func(b, message):
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**ᴍᴀᴅᴇ ʙʏ: @mkn_bots_updates**",
+        caption="**ᴍᴀᴅᴇ ʙʏ: @DIFTANN**",
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ꜱᴜᴩᴩᴏʀᴛ ᴜꜱ", url="https://t.me/mkn_bots_updates")]]),                   
     )
     await m.delete()
