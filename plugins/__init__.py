5d20cfca27b7a011932f4cd1f533c9d7e88f5f85
xyz148 <zxy30730@gmail.com>
2024-05-17 00:09:12 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -10,7 +10,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response(text="ᴍᴀᴅᴇ ʙʏ: Mᴋɴ Bᴏᴛs™")
+    return web.json_response(text="ᴍᴀᴅᴇ ʙʏ『𝗛𝗘𝗠𝗨』 Bᴏᴛs™")
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
