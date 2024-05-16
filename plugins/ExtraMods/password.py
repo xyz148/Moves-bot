0bb5183d57289a8e3a1da2d8c9989390a37d6c37
xyz148 <zxy30730@gmail.com>
2024-05-17 00:02:15 +0530

    Update password.py

--- plugins/ExtraMods/password.py
+++ plugins/ExtraMods/password.py
@@ -15,5 +15,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Lɪᴍɪᴛ:</b> {str(limit)} \n<b>Pᴀꜱꜱᴡᴏʀᴅ: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Mᴋɴ Bᴏᴛᴢ™️', url='https://t.me/mkn_bots_updates')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('『𝗛𝗘𝗠𝗨』', url='https://t.me/movess_hub')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
